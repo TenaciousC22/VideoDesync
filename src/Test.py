f0650df3ae5c1b0757a7a00a457c48c82da8cc73
Chris Perlette <chrisperlette@gmail.com>
2019-10-10 18:21:41 -0600

    Adding files

---
+++ src/Test.py
@@ -0,0 +1,10 @@
+from moviepy.editor import *
+from subroutines import *
+
+#Variables for functions
+offsets=["060","240","360"]
+clips=importSilence()
+
+if __name__=="__main__":
+	#createAudioOffsets(clips, offsets, "../sound/bin/test/", "whiteNoise", ".wav")
+	
---
+++ src/subroutines.py
@@ -0,0 +1,33 @@
+from moviepy.editor import *
+
+#Import Silence buffers
+def importSilence():
+	clips=[]
+	clips.append(AudioFileClip("../sound/bin/silence/060.wav"))
+	clips.append(AudioFileClip("../sound/bin/silence/240.wav"))
+	clips.append(AudioFileClip("../sound/bin/silence/360.wav"))
+	return clips
+
+#Creates all offset audio files (+ and - offsets)
+#Variables are as follows
+#clips: array of clips for padding
+#offsets: array of stings of the offsets in milliseconds
+#path: file path to audio file you are modifying
+#aFile: name of the audio file without the extention
+#fType: file extention, include the '.' e.g. ".wav"
+def createAudioOffsets(clips, offsets, path, aFile, fType):
+	clip=AudioFileClip(path+aFile+fType)
+	for i in range(0,len(clips)):
+		writePad(clip,clips[i],path,aFile,offsets[i],fType)
+		writeCut(clip,path,aFile,offsets[i],fType)
+	return
+
+#helper function for createAudioOffsets
+def writePad(clip, pad, path, aFile, mod, fType):
+	modclip=concatenate_audioclips([pad,clip])
+	modclip.write_audiofile(path+aFile+"+"+mod+fType)
+
+#helper function for createAudioOffsets
+def writeCut(clip, path, aFile, mod, fType):
+	modclip=clip.subclip("00:00:00."+mod)
+	modclip.write_audiofile(path+aFile+"-"+mod+fType)
