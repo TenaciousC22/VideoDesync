09ccb685c0fbc0f5bb62ea972b0c599de2ff490d
Chris Perlette <chrisperlette@gmail.com>
2019-10-10 19:55:40 -0600

    one of these days I will get good at commiting

--- src/Test.py
+++ src/Test.py
@@ -1,10 +1,12 @@
 from moviepy.editor import *
 from subroutines import *
+import csv
 
 #Variables for functions
 offsets=["060","240","360"]
 clips=importSilence()
 
 if __name__=="__main__":
-	#createAudioOffsets(clips, offsets, "../sound/bin/test/", "whiteNoise", ".wav")
-	
+	#createAudioOffsets(clips, offsets, "../bin/sound/test/", "whiteNoise", ".wav")
+	createSubclips("../bin/video/test/", "test", ".mp4")
+	#VideoFileClip("../bin/video/test/test.mp4")
--- src/subroutines.py
+++ src/subroutines.py
@@ -1,11 +1,12 @@
 from moviepy.editor import *
+import csv
 
 #Import Silence buffers
 def importSilence():
 	clips=[]
-	clips.append(AudioFileClip("../sound/bin/silence/060.wav"))
-	clips.append(AudioFileClip("../sound/bin/silence/240.wav"))
-	clips.append(AudioFileClip("../sound/bin/silence/360.wav"))
+	clips.append(AudioFileClip("../bin/sound/silence/060.wav"))
+	clips.append(AudioFileClip("../bin/sound/silence/240.wav"))
+	clips.append(AudioFileClip("../bin/sound/silence/360.wav"))
 	return clips
 
 #Creates all offset audio files (+ and - offsets)
@@ -31,3 +32,32 @@ def writePad(clip, pad, path, aFile, mod, fType):
 def writeCut(clip, path, aFile, mod, fType):
 	modclip=clip.subclip("00:00:00."+mod)
 	modclip.write_audiofile(path+aFile+"-"+mod+fType)
+
+def createSubclips(path, vFile, fType):
+	intervals=extractIntervals(path)
+	for x in range(len(intervals)):
+		clip=VideoFileClip(path+vFile+fType)
+		if x==len(intervals)-1:
+			sub=clip.subclip(intervals[x])
+		else:
+			sub=clip.subclip(intervals[x][0],intervals[x][1])
+		sub.write_videofile(path+"subclips/"+vFile+"_subclip"+str(x+1)+fType)
+
+def extractIntervals(path):
+	holder=open(path+"intervals.csv","r")
+	intervals=[]
+	y=0
+	for line in holder:
+		raw=line
+		for x in range(len(raw)):
+			if raw[x]==",":
+				intervals.append(raw[y:x-1])
+				y=x+1
+	intervals.append(raw[y:len(raw)-1])
+	holder=[]
+	for x in range(len(intervals)-1):
+		holder.append([intervals[x],intervals[x+1]])
+	holder.append(intervals[len(intervals)-1])
+	intervals=holder
+	#print(intervals)
+	return intervals
